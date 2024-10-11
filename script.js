adc4eeb9cb8afc7082eb767fec6c2b98345678d2
Premanshu Ray <93930405+Prem-Ray@users.noreply.github.com>
2024-10-11 22:03:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// generate random color
+
+const RandomColor = function(){
+    const hexItems = '0123456789ABCDEF' ;
+    let color = '#' ;
+    
+    for(let i=0 ; i<6 ; i++){
+        color += hexItems[Math.floor(Math.random()*16)] ;
+        // console.log(color) ;
+    }
+    return color ;
+}
+
+let interval ;
+
+const startChangingColor = function(){
+    
+    if(!interval)
+        interval = setInterval(changeColor,700) ;
+
+    function changeColor(){
+        document.body.style.backgroundColor = RandomColor()
+    }
+    
+}
+
+
+document.querySelector('#start').addEventListener('click',startChangingColor)
+
+const stopChangingColor = function(){
+    clearInterval(interval) ;
+    interval = null ;
+}
+
+document.querySelector('#stop').addEventListener('click',stopChangingColor)
+
